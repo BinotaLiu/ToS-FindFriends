ab4a794bfcdc2968d01343981f260d108d5bc8b0
BinotaLiu <binota@binota.org>
2014-05-04 02:40:00 +0800

    新增三個檢視器

---
+++ var/view/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="utf-8">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?=$data['title']?></title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+    <style type="text/css">
+      body {
+        background: #EEE;
+      }
+    </style>
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar>
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="#"><?=$data['nav_title']?></a></h1>
+        </li>
+      </ul>
+    </nav>
