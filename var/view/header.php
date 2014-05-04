fb803920d8f5da10325992f3bae5a98e4945daad
BinotaLiu <binota@binota.org>
2014-05-04 22:27:52 +0800

    修正檢視器 header.php 中的連結

--- var/view/header.php
+++ var/view/header.php
@@ -17,7 +17,7 @@
     <nav class="top-bar" data-topbar>
       <ul class="title-area">
         <li class="name">
-          <h1><a href="#"><?=$data['nav_title']?></a></h1>
+          <h1><a href="index.php"><?=$data['nav_title']?></a></h1>
         </li>
         <li class="toggle-topbar menu-icon"><a href="#"><span>您好，<?php if($loginStatus){ echo $data['userName']; }else{?>遊客<?php } ?>！</span></a></li>
       </ul>
@@ -27,7 +27,7 @@
           <li class="show-for-medium-up"><a href="home.php">您好，<?=$data['userName']?>！</a></li>
           <li><a href="setting.php">個人設定</a></li>
           <li><a href="mycard.php">編輯代表資訊</a></li>
-          <li><a href="logout">登出</a></li>
+          <li><a href="login.php?logout=true">登出</a></li>
 <?php else: ?>
           <li><a href="login.php?method=facebook">Facebook 登入</a></li>
           <li><a href="login.php?method=persona">Persona 登入</a></li>
