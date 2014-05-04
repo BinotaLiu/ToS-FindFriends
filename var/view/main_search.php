ad91060630f83ac5b15a8bb27a6321412c578dda
BinotaLiu <binota@binota.org>
2014-05-05 04:33:11 +0800

    讓檢視器 main_search.php 可以搜尋

--- var/view/main_search.php
+++ var/view/main_search.php
@@ -7,12 +7,12 @@
         <h3>馬上來徵戰友吧！</h3>
         <p>預見先鋒徵戰友系統強勢上線！搭載最先進最好用的徵戰友系統，幫你找到最適合的戰友！</p>
         <div class="clear"></div>
-        <form>
+        <form action="search.php" method="get">
           <div class="row">
             <div class="large-6 column">
               <div class="row collapse">
                 <div class="small-7 column">
-                  <select>
+                  <select name="card">
                     <option value="0">-- 選擇卡片 --</option>
                     <?php showCardList(null); ?>
                   </select>
