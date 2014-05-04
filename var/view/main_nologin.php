8f113725c464640a612304e9b6d02d16196d7393
BinotaLiu <binota@binota.org>
2014-05-04 16:42:57 +0800

    拆分檢視器 main.php

---
+++ var/view/main_nologin.php
@@ -0,0 +1,8 @@
+      <div class="large-12 panel column">
+        <h3>刊登自己的代表</h3>
+        <p>請先登入</a>
+        <div class="clear"></div>
+        <a class="button medium radius" href="./login.php">登入/註冊</a>
+        </div>
+      </div>
+    </div>
---
+++ var/view/main_search.php
@@ -0,0 +1,35 @@
+    <div class="row">
+      <div class="large_12 column">
+        <h1 class="centered">徵戰友</h1>
+      </div>
+      <div class="large-12 panel column">
+        <h3>馬上來徵戰友吧！</h3>
+        <p>預見先鋒徵戰友系統強勢上線！搭載最先進最好用的徵戰友系統，幫你找到最適合的戰友！</p>
+        <div class="clear"></div>
+        <form>
+          <div class="row">
+            <div class="large-6 column">
+              <div class="row collapse">
+                <div class="small-7 column">
+                  <select>
+                    <option value="0">-- 選擇卡片 --</option>
+                  </select>
+                </div>
+                <div class="small-5 column">
+                  <button class="postfix" type="submit" value="0">找戰友！</button>
+                </div>
+              </div>
+            </div>
+            <div class="large-6 column">
+              <div class="row collapse">
+                <div class="small-7 column">
+                  <input type="text" placeholder="依 ID 搜尋"></input>
+                </div>
+                <div class="small-5 column">
+                  <button class="postfix" type="submit" value="1">找戰友！</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form> 
+      </div>
