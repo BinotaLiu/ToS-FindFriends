cad52b1478c89b6d84f27a258679955f73f0824d
BinotaLiu <binota@binota.org>
2014-05-05 00:17:48 +0800

    完成設定頁

---
+++ setting.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Filename: setting.php
+ * $Id$
+ * 設定頁，用於提供使用者進行設定。
+ */
+define('IN_MOUGE', true);
+
+session_start();
+
+require_once 'config.inc.php';
+require_once 'src/database.php';
+
+$db = new Database;
+$db->connect($config['database']['host'], $config['database']['user'], $config['database']['passwd']);
+$db->select($config['database']['name']);
+
+
+//检查用户登录状态
+if(!empty($_SESSION['user_id']) && !empty($_SESSION['user_token'])){
+  $checkToken = md5($config['secret']['key'][1] . md5($_SESSION['user_id'] . $config['secret']['key'][0]));
+  if($_SESSION['user_token'] == $checkToken)
+    $loginStatus = 1;
+  else{
+    $loginStatus = 0;
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_token']);
+    unset($_SESSION['user_name']);
+  }
+}else{
+  $loginStatus = 0;
+}
+
+if(!$loginStatus) die("Please login first!");
+
+$data['title'] = "徵戰友 | 遇見先鋒 - Powered by MouGE";
+$data['nav_title'] = "徵戰友";
+if($loginStatus){
+  $data['uid'] = $_SESSION['user_id'];
+  $data['userName'] = $_SESSION['user_name'];
+}
+
+$myInfo = $db->fetch_where('user', array('*'), array('uid' => $data['uid']));
+
+if($myInfo[0]['facebook_id'])
+  $data['form']['loginmethod'] = "Facebook (UID:" . $myInfo[0]['facebook_id'] . ")";
+else
+  $data['form']['loginmethod'] = "Persona (E-Mail: " . $myInfo[0]['email'] . ")";
+
+if(!empty($_POST['nickname'])){
+  $db->update('user', array('nickname' => $_POST['nickname']), array('uid' => $data['uid']));
+  $fixed_nickname = $db->fix_string($_POST['nickname']);
+  $data['form']['nickname'] = $fixed_nickname;
+  //复写SESSION 中的资讯：
+  $_SESSION['user_name'] = $fixed_nickname;
+  $data['userName'] = $fixed_nickname;
+}else
+  $data['form']['nickname'] = $myInfo[0]['nickname'];
+
+include 'var/view/header.php';
+
+include 'var/view/setting.php';
+
+include 'var/view/footer.php';
+
---
+++ var/view/setting.php
@@ -0,0 +1,35 @@
+
+<div class="row">
+    <div class="small-12 column">
+      <h1>設定</h1>
+    </div>
+    <div class="clear"></div>
+    <div class="small-12 panel column">
+      <h3>個人資料</h3>
+      <div class="row">
+        <form action="setting.php" method="post">
+          <div class="small-12 column">
+            <div class="row">
+              <div class="small-3 column">
+                <label class="right inline" for="loginmethod">登入方式</label>
+              </div>
+              <div class="small-9 column">
+                <input type="text" id="loginmethod" disabled="disabled" value="<?=$data['form']['loginmethod']?>">
+              </div>
+            </div>
+            <div class="row">
+              <div class="small-3 column">
+                <label class="right inline" for="nickname">昵稱</label>
+              </div>
+              <div class="small-9 column">
+                <input type="text" id="nickname" name="nickname" value="<?=$data['form']['nickname']?>">
+              </div>
+            </div>
+          </div>
+          <button class="small-4 column medium" type="submit">保存</button>
+        </form>
+      </div>
+    </div>
+
+</div>
+
