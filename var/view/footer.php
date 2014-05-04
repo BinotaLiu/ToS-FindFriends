8ba1046402161f463380adc4a881bbabfdee2ecd
BinotaLiu <binota@binota.org>
2014-05-05 04:55:39 +0800

    修改檢視器 footer.php

--- var/view/footer.php
+++ var/view/footer.php
@@ -1,3 +1,10 @@
+
+    <div class="row">
+      <div class="small-11 small-offset-1 medium-offset-0 column">
+        <h6 style="color:#777;font-size:.7em;">2012-2014 &copy; Studio-MouGE, All Right Reserved.<br>Online Version: <?=$config['system']['version']?></h6>
+      </div>
+    </div>
+
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
     <script>
