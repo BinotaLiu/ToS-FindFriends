700e9ae51885bf656465a30e2ced321f3b5105a0
BinotaLiu <binota@binota.org>
2014-05-05 16:50:58 +0800

    修正檢視器中的一些錯誤並加入Google 統計代碼

--- var/view/main_nologin.php
+++ var/view/main_nologin.php
@@ -1,6 +1,6 @@
       <div class="large-12 panel column">
         <h3>刊登自己的代表</h3>
-        <p>請先登入</a>
+        <p>請先登入</p>
         <div class="clear"></div>
           <ul class="inline-list">
             <li><a class="button large radius" href="login.php?method=facebook">Facebook 登入</a></li>
