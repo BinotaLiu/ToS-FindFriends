ee6698ea7dcbebc585bacd3c4125e4880a5c35f5
BinotaLiu <binota@binota.org>
2014-05-06 23:46:00 +0800

    公告加入 Disqus 支援

--- views/topic_content.php
+++ views/topic_content.php
@@ -14,5 +14,17 @@ if(!defined('IN_MOUGE'))
       <?=$data['topic']['content']?>
     </div>
   </div>
+    <div id="disqus_thread" class="small-10 small-offset-1 medium-12 medium-offset-0 column"></div>
+    <script type="text/javascript">
+        var disqus_shortname = '<?=$config['social']['disqus']['shortname']?>';
+
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>請啟用 JavaScript 以檢視來自<a href="http://disqus.com/?ref_noscript">Disqus</a>的留言</noscript>
+    <a href="http://disqus.com" class="dsq-brlink">留言功能自豪地採用 <span class="logo-disqus">Disqus</span></a>
 </div>
 
