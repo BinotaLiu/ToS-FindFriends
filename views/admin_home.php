5c3dc8673145e4ffc13ab2cd5065ddf41c6bd388
BinotaLiu <binota@binota.org>
2014-05-07 17:51:15 +0800

    修改了後台首頁的連結樣式

--- views/admin_home.php
+++ views/admin_home.php
@@ -9,13 +9,11 @@ if(!defined('IN_MOUGE'))
   </div>
   <div class="small-12 panel column">
     <h3>早安，<?=$data['userName']?></h3>
-    這裡是管理中心，辛苦了，<br>
-    請選擇您要進行的項目：
-    <ul>
-      <li><a href="admin.php?mod=topic">管理公告</a></li>
-      <li><a href="admin.php?mod=user">管理使用者</a></li>
-      <li><a href="index.php">回到網站</a></li>
-      <li><a href="login.php?logout=true">登出</a></li>
+    <ul class="inline-list">
+      <li><a class="button large radius" href="admin.php?mod=topic">管理公告</a></li>
+      <li><a class="button large radius" href="admin.php?mod=user">管理使用者</a></li>
+      <li><a class="button large radius" href="index.php">回到網站</a></li>
+      <li><a class="button large radius success" href="login.php?logout=true">登出</a></li>
     </ul>
   </div>
 </div>
